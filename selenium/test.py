88f54809d3d7c4df0c18cae5aff8c4e077777978
Astakhov Sergey <fzastahov@gmail.com>
2021-10-19 23:03:36 +0300

    cards testing

--- selenium/test.py
+++ selenium/test.py
@@ -4,6 +4,17 @@ import time
 import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+def click_card_btn(browser, tab_locator, btn_locator):
+    tab = browser.find_element(*tab_locator)
+    tab.click()
+    btn = browser.find_element(*btn_locator)
+    btn.click()
+    browser.refresh()
+    time.sleep(0.1)
+    assert 0 == len(browser.find_elements(*btn_locator))
+
 
 @pytest.mark.skip("skip")
 def test_login(browser):
@@ -31,3 +42,17 @@ def test_tabs(browser, tab_locator, page_url):
     tab = browser.find_element(*tab_locator)
     tab.click()
     assert browser.current_url.count(page_url) == 1
+
+"""
+1 - add Aleksandr Petrov
+2 - delete him from friends
+"""
+def test_accept_delete_friend(browser):
+    browser.implicitly_wait(0.5)
+    accept_btn_locator = (By.CSS_SELECTOR, """button[onclick="send_ajax(3, 'accept_incoming')"]""")
+    click_card_btn(browser, locators.TAB_IN_LOCATOR, accept_btn_locator)
+    delete_btn_locator = (By.CSS_SELECTOR, \
+        """button[onclick="send_ajax(3, 'delete_friend')"]""")
+    click_card_btn(browser, locators.TAB_FRIENDS_LOCATOR, delete_btn_locator)
+    time.sleep(3)
+
