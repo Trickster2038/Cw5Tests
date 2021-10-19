a2396ec131af36b9c2c63e03d554c697bf741574
Astakhov Sergey <fzastahov@gmail.com>
2021-10-19 22:29:39 +0300

    tebs tests

--- selenium/locators.py
+++ selenium/locators.py
@@ -5,3 +5,11 @@ LOGIN_ID_FIELD_LOCATOR = (By.ID, 'id_username')
 PASSWORD_FIELD_LOCATOR = (By.ID, 'id_password')
 LOGIN_FORM_BTN_LOCATOR = (By.CSS_SELECTOR, 'button[type="submit"]')
 LOGOUT_BTN_LOCATOR = (By.CSS_SELECTOR, 'a[href="/logout/"]')
+
+TAB_OUT_LOCATOR = (By.CSS_SELECTOR, 'a[href="/outgoing/"]')
+TAB_IN_LOCATOR = (By.CSS_SELECTOR, 'a[href="/incoming/"]')
+TAB_FRIENDS_LOCATOR = (By.CSS_SELECTOR, 'a[href="/friends/"]')
+TAB_SEARCH_LOCATOR = (By.CSS_SELECTOR, 'a[href="/search?safe=false"]')
+TAB_SAFESEARCH_LOCATOR = (By.CSS_SELECTOR, 'a[href="/search?safe=true"]')
+TAB_MODERATE_LOCATOR = (By.CSS_SELECTOR, 'a[href="/moderate/"]')
+# a[href="/search?safe=true"]
--- selenium/test.py
+++ selenium/test.py
@@ -1,16 +1,33 @@
 from selenium import webdriver
 import locators
 import time
+import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.mark.skip("skip")
 def test_login(browser):
     assert len(browser.find_elements(*locators.LOGOUT_BTN_LOCATOR)) != 0
 
+@pytest.mark.skip("skip")
 def test_logout(browser):
     logout_btn = browser.find_element(*locators.LOGOUT_BTN_LOCATOR)
     logout_btn.click()
     assert len(browser.find_elements(*locators.LOGIN_BTN_LOCATOR)) != 0
 
-def test_tabs(browser):
-    pass
+@pytest.mark.skip("skip")
+@pytest.mark.parametrize(
+        'tab_locator, page_url',
+        [
+            pytest.param(locators.TAB_OUT_LOCATOR, '/outgoing'),
+            pytest.param(locators.TAB_IN_LOCATOR, '/incoming'),
+            pytest.param(locators.TAB_FRIENDS_LOCATOR, '/friends'),
+            pytest.param(locators.TAB_SEARCH_LOCATOR, '/search/?safe=false'),
+            pytest.param(locators.TAB_SAFESEARCH_LOCATOR, '/search/?safe=true'),
+            pytest.param(locators.TAB_MODERATE_LOCATOR, '/moderate'),
+        ]
+    )
+def test_tabs(browser, tab_locator, page_url):
+    tab = browser.find_element(*tab_locator)
+    tab.click()
+    assert browser.current_url.count(page_url) == 1
